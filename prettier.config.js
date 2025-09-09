271f059c8ab8b0ab0d37061900201a03c6e7baca
Dmitry Sharov <frozik@gmail.com>
2025-09-09 18:15:14 +0300

    chore: clean dependencies

---
+++ prettier.config.js
@@ -0,0 +1,21 @@
+/**
+ * @see https://prettier.io/docs/en/configuration.html
+ * @type {import("prettier").Config}
+ */
+const config = {
+  semi: true,
+  trailingComma: 'all',
+  singleQuote: true,
+  printWidth: 100,
+  tabWidth: 4,
+  overrides: [
+    {
+      files: ['*.json', '*.config.js'],
+      options: {
+        tabWidth: 2,
+      },
+    },
+  ],
+};
+
+export default config;
